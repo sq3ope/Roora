aa8aaa5543258ee4147a716df96e2ea3a08818ac
Pawel Firlej <sq3ope@gmail.com>
2011-07-15 22:17:44 +0200

    added roo files

---
+++ src/test/java/org/roora/domain/PersonStatusIntegrationTest.java
@@ -0,0 +1,12 @@
+package org.roora.domain;
+
+import org.junit.Test;
+import org.springframework.roo.addon.test.RooIntegrationTest;
+
+@RooIntegrationTest(entity = PersonStatus.class)
+public class PersonStatusIntegrationTest {
+
+    @Test
+    public void testMarkerMethod() {
+    }
+}
---
+++ src/test/java/org/roora/domain/ProductStatusIntegrationTest.java
@@ -0,0 +1,12 @@
+package org.roora.domain;
+
+import org.junit.Test;
+import org.springframework.roo.addon.test.RooIntegrationTest;
+
+@RooIntegrationTest(entity = ProductStatus.class)
+public class ProductStatusIntegrationTest {
+
+    @Test
+    public void testMarkerMethod() {
+    }
+}
