a80eef0ab512d139df7595eafb2910b72d6b5b56
Pawel Firlej <email>
2012-09-14 22:09:26 +0200

    first tomcat version

---
+++ src/main/java/org/roora/domain/Product.java
@@ -0,0 +1,20 @@
+package org.roora.domain;
+
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotNull;
+import org.springframework.roo.addon.javabean.RooJavaBean;
+import org.springframework.roo.addon.jpa.activerecord.RooJpaActiveRecord;
+import org.springframework.roo.addon.tostring.RooToString;
+
+@RooJavaBean
+@RooToString
+@RooJpaActiveRecord
+public class Product {
+
+    @NotNull
+    private String name;
+
+    @NotNull
+    @ManyToOne
+    private Unit unit;
+}
---
+++ src/test/java/org/roora/domain/ProductDataOnDemand.java
@@ -0,0 +1,7 @@
+package org.roora.domain;
+
+import org.springframework.roo.addon.dod.RooDataOnDemand;
+
+@RooDataOnDemand(entity = Product.class)
+public class ProductDataOnDemand {
+}
