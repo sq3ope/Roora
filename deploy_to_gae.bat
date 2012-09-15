11344ade26eb73bb8103595244e8fabf52f40ad1
Pawel Firlej <email>
2012-09-15 21:41:37 +0200

    Enhanced deploy_to_gae script

--- deploy_to_gae.bat
+++ deploy_to_gae.bat
@@ -1,1 +1,5 @@
-C:\%HOMEPATH%\.m2\repository\com\google\appengine\appengine-java-sdk\1.6.5\appengine-java-sdk-1.6.5\bin\appcfg.cmd update target/Roora-0.1.0.BUILD-SNAPSHOT
+set APP_ENGINE_VERSION=1.6.5
+set ARTIFACT_NAME=Roora
+set ARTIFACT_VERSION=0.1.0.BUILD-SNAPSHOT
+
+C:%HOMEPATH%\.m2\repository\com\google\appengine\appengine-java-sdk\%APP_ENGINE_VERSION%\appengine-java-sdk-%APP_ENGINE_VERSION%\bin\appcfg.cmd update target/%ARTIFACT_NAME%-%ARTIFACT_VERSION%
